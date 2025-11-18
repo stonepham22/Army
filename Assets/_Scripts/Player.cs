b419d2d3c34fea715690c00e495afbe47dd4016d
lobozinho stone <stonepham22>
2025-11-18 15:45:21 +0700

    Done player animation

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Animator _animator;
     [SerializeField] private float _input;
     [SerializeField] private float _moveSpeed = 5f;
 
     void Reset()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
+
     void FixedUpdate()
     {
         GetInput();
         Move();
+        PlayAnimation();
     }
 
     void GetInput()
@@ -26,6 +30,11 @@ public class Player : MonoBehaviour
         if(_input==0) return;
         _rigidbody.linearVelocity = new Vector3(_input * _moveSpeed, _rigidbody.linearVelocity.y, 0);
         transform.localScale = new Vector3(Mathf.Sign(Input.GetAxis("Horizontal")), 1, 1);
+    }
+
+    void PlayAnimation()
+    {
+        _animator.Play(_input != 0 ? "Walk" : "Idle");
     }
 
 }
