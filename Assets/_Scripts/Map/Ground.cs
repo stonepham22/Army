e020b3118dbf9f7bb158c3eebc251571f2588500
lobozinho stone <stonepham22>
2025-11-19 17:38:22 +0700

    Update Ground

--- Assets/_Scripts/Map/Ground.cs
+++ Assets/_Scripts/Map/Ground.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    [SerializeField] private float _distance = 4f;
+    
     void FixedUpdate()
     {
         float camPosX = Camera.main.transform.position.x;
         
-        if(math.abs(camPosX - transform.position.x) > 8f)
+        if(math.abs(camPosX - transform.position.x) > _distance)
         {
             transform.position = new Vector3(camPosX, transform.position.y, transform.position.z);
         }
