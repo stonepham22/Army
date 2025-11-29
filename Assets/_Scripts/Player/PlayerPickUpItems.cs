6d65ddd446fb5f77d87b31cc07f0d71dafc028e0
lobozinho stone <stonepham22>
2025-11-29 18:51:06 +0700

    Done Player UI

--- Assets/_Scripts/Player/PlayerPickUpItems.cs
+++ Assets/_Scripts/Player/PlayerPickUpItems.cs
@@ -1,11 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerPickUpItems : MonoBehaviour
 {
     [SerializeField] private int _gold = 0;
     [SerializeField] private int _iron = 0;
-    
+
+    [SerializeField] private Text _goldText;
+    [SerializeField] private Text _ironText;
+
+    void Awake()
+    {
+        _goldText.text = "Gold: " + _gold.ToString();
+        _ironText.text = "Iron: " + _iron.ToString();
+    }   
     
     private void OnTriggerEnter2D(Collider2D other) {
         
@@ -20,8 +29,10 @@ public class PlayerPickUpItems : MonoBehaviour
     private void AddItem(GameObject item) {
         if(item.name.Contains("Gold")) {
             _gold += 1;
+            _goldText.text = "Gold: " + _gold.ToString();
         } else if(item.name.Contains("Iron")) {
             _iron += 1;
+            _ironText.text = "Iron: " + _iron.ToString();
         }
     }
 
