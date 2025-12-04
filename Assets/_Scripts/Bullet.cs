8f42cebe4d2cce57b52f7d6ee99a4ae6c93f71bc
lobozinho stone <stonepham22>
2025-12-04 11:14:47 +0700

    Done shoot player

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -2,21 +2,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private int _damage = 20;
     public Vector2 direction;
 
     void FixedUpdate()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // Xử lý va chạm với enemy hoặc các đối tượng khác
-        if (collision.CompareTag("Enemy"))
-        {
-            collision.GetComponent<IDamageReceiver>()?.ReceiveDamage(20);
-            Destroy(gameObject); // Hủy bullet sau khi va chạm        
-        }
+        collision.GetComponent<IDamageReceiver>()?.ReceiveDamage(_damage);
+        Destroy(gameObject); // Hủy bullet sau khi va chạm      
     }
 }
